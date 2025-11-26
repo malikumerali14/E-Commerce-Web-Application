1a2d5495d080c443c5ceaeae83c0715a629b807d
malikumerali14 <168172046+malikumerali14@users.noreply.github.com>
2025-11-26 10:21:14 +0500

    Fix environment variable name for server port

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use((req, res, next) => {
     // This will run on every request
