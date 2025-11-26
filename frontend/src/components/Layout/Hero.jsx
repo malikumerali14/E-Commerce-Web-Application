6ef1fad7f2715b7d47ee1f9304eabe03530d17bc
malikumerali14 <168172046+malikumerali14@users.noreply.github.com>
2025-11-26 13:05:59 +0500

    Update Hero component layout for text positioning
    Adjusted the positioning of the animated text container in the Hero component.

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <>
         <section className='relative bg-black'>
           <img src={Hero_Image} className=' h-[400px] md:h-[550px] lg:h-[650px]  ' />
-          <div className='  animate__animated animate__fadeInDown animate_slower absolute font-semibold bottom-[30%] md:bottom-[45%] right-[13%] sm:right-[11%] md:right-[5%] md:font-extrabold sm:w-[25%] md:w-[45%] lg:w-[50%] rounded-md text-center py-1 lg:py-2 tracking-widest transition-all hover:cursor-pointer duration-300 '>
+          <div className='  animate__animated animate__fadeInDown animate_slower absolute font-semibold bottom-[30%] md:bottom-[60%] right-[13%] sm:right-[11%] md:right-[5%] md:font-extrabold sm:w-[25%] md:w-[45%] lg:w-[50%] rounded-md text-center py-1 lg:py-2 tracking-widest transition-all hover:cursor-pointer duration-300 '>
             <h1 className='text-transparent text-3xl sm:text-4xl md:text-6xl lg:text-7xl [-webkit-text-stroke:0.05px_red]'>When AR Meets Style</h1>
           </div>
           <div className='  animate__animated animate__fadeInDown animate_slower absolute md:block bottom-[25%] sm:bottom-[15%] md:bottom-[30%] lg:bottom-[40%] lg:right-[23.5%] md:right-[20%] sm:right-[18%] right-[40%] text-gray-300 bg-red-800 text-xl sm:w-28 md:w-32 lg:w-44 rounded-sm text-center py-1 lg:py-2 tracking-widest hover:-translate-y-2 transition-all hover:cursor-pointer duration-300 font-semibold  '>
